31fc56c26e3ae4aede1db1f7e53883907544e299
roman-glushkov <roma.glushkov.vuz@mail.ru>
2026-02-17 19:52:01 +0300

    1 лаба 1 задание

---
+++ 1 lab/1 task 1 option/src/test.bat
@@ -0,0 +1,45 @@
+@echo off
+
+set BASE_DIR=%~dp0
+set TEST_DIR=%BASE_DIR%tests
+set OUTPUT_DIR=%BASE_DIR%output
+
+if not exist "%OUTPUT_DIR%" mkdir "%OUTPUT_DIR%"
+
+set PROGRAM="%~1"
+if "%~1"=="" set PROGRAM="%BASE_DIR%copyfile.exe"
+
+rem Тест 1 – пустой файл
+%PROGRAM% "%TEST_DIR%\test-empty.txt" "%OUTPUT_DIR%\empty.txt"
+if ERRORLEVEL 1 goto err
+fc.exe "%OUTPUT_DIR%\empty.txt" "%TEST_DIR%\test-empty.txt"
+if ERRORLEVEL 1 goto err
+
+rem Тест 2 – одна строка
+%PROGRAM% "%TEST_DIR%\test-hello.txt" "%OUTPUT_DIR%\hello.txt"
+if ERRORLEVEL 1 goto err
+fc.exe "%OUTPUT_DIR%\hello.txt" "%TEST_DIR%\test-hello.txt"
+if ERRORLEVEL 1 goto err
+
+rem Тест 3 – много строк
+%PROGRAM% "%TEST_DIR%\test-poem.txt" "%OUTPUT_DIR%\poem.txt"
+if ERRORLEVEL 1 goto err
+fc.exe "%OUTPUT_DIR%\poem.txt" "%TEST_DIR%\test-poem.txt"
+if ERRORLEVEL 1 goto err
+
+rem Тест 4 – числа
+%PROGRAM% "%TEST_DIR%\test-numbers.txt" "%OUTPUT_DIR%\numbers.txt"
+if ERRORLEVEL 1 goto err
+fc.exe "%OUTPUT_DIR%\numbers.txt" "%TEST_DIR%\test-numbers.txt"
+if ERRORLEVEL 1 goto err
+
+rem Тест 5 – отсутствующий файл
+%PROGRAM% "%TEST_DIR%\missing.txt" "%OUTPUT_DIR%\missing.txt" > "%OUTPUT_DIR%\error.txt"
+if NOT ERRORLEVEL 1 goto err
+
+echo Program testing succeeded
+exit 0
+
+:err
+echo Program testing failed
+exit 1
